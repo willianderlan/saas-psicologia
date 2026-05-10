e6cb3f5d165c56c5278f853596f080922128ddbf
Willian Derlan <willianderlancnphotmail.com@gmail.com>
2026-05-10 20:07:39 -0300

    Atualizações no sistema calendario

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,24 +5,26 @@ from pacientes.models import Paciente
 from agenda.models import Sessao
 from financeiro.models import Pagamento
 
+from django.utils import timezone
+
 
 @login_required
 def home(request):
 
-    total_pacientes = Paciente.objects.filter(
-        usuario=request.user
+    total_pacientes = Paciente.objects.do_usuario(
+        request.user
     ).count()
 
-    total_sessoes = Sessao.objects.filter(
-        paciente__usuario=request.user
+    total_sessoes = Sessao.objects.do_usuario(
+        request.user
     ).count()
 
-    total_pagamentos = Pagamento.objects.filter(
-        sessao__paciente__usuario=request.user
+    total_pagamentos = Pagamento.objects.do_usuario(
+        request.user
     ).count()
 
-    pagamentos = Pagamento.objects.filter(
-        sessao__paciente__usuario=request.user
+    pagamentos = Pagamento.objects.do_usuario(
+        request.user
     )
 
     total_faturado = sum(
@@ -30,12 +32,17 @@ def home(request):
         for pagamento in pagamentos
     )
 
+    proximas_sessoes = Sessao.objects.do_usuario(
+        request.user
+    ).order_by('data', 'hora')[:5]
+
     context = {
 
         'total_pacientes': total_pacientes,
         'total_sessoes': total_sessoes,
         'total_pagamentos': total_pagamentos,
         'total_faturado': total_faturado,
+        'proximas_sessoes': proximas_sessoes,
 
     }
 
